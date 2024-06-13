7457d84d2a5499197672da8cb987d2f8e49a2077
tamdesu <myfilesharing150@gmail.com>
2024-06-13 11:22:21 +0000

    logo

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,8 +3,8 @@ import styles from "@/app/globals.css";
 
 export default function Glitch() {
   return (
-      <nav className="w-full h-[60px] bg-black bg-opacity-30 backdrop-blur drop-shadow flex items-center p-2  text-xl font-thin fixed top-0 left-0 z-50">
-      <span>RUET CSE • <span className="text-cyan-300">23</span></span>
+      <nav className="w-full h-[60px] bg-black bg-opacity-30 backdrop-blur drop-shadow flex items-center p-2  text-xl font-extralight fixed top-0 left-0 z-50">
+      <span><span className="text-cyan-500 font-normal">{"</>"}</span> RUET CSE • <span className="text-cyan-300">23</span></span>
       </nav>
   );
 }
